7df71d08366aa5178ce4b9f872d9da2a311bed02
fatima hassan <fatimahasan774@gmail.com>
2026-04-20 06:01:04 +0300

    addin ex-4

--- ex.js
+++ ex.js
@@ -1,4 +1,36 @@
-let isSanny =true;
-let weekend =true;
+// let isSanny =true;
+// let weekend =true;
 
-console.log(isSanny && weekend);
+// console.log(isSanny && weekend);
+
+
+// function add(a,b){
+//         console.log(a+b)
+// }
+// add(20,30);
+// add(100,200);
+
+//exercise 4;
+function add(a,b){
+        return a+b
+}
+console.log(add (10,10));
+console.log(add (5,10));
+console.log(add (8,10));
+console.log(add (2,1));
+console.log(add (19,90));
+
+//2 experstion function
+
+let add =function(a,b){
+    return a+b
+  
+}
+
+ console.log(add(10,10));
+ console.log(add(20,30));
+ console.log(add(5,5));
+
+
+
+

54ab30410075b247ee66f3ac1e146d030b9b6463
fatima hassan <fatimahasan774@gmail.com>
2026-04-21 11:21:58 +0300

    upload exercise 6

--- ex.js
+++ ex.js
@@ -1,7 +1,22 @@
 let colors =['red', 'white', 'black'];
-console.log(colors[1]);
-console.log(colors[2]);
-console.log(colors[0]);
+console.log(colors);
+
+console.log('push')
+colors.push('blue');
+console.log(colors)
+
+console.log('pop')
+colors.pop()
+console.log(colors)
+
+
+console.log('unshift')
+colors.unshift('cyan')
+console.log(colors)
+
+console.log('shift')
+colors.shift()
+console.log(colors)
 
 
 

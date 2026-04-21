93fdb8912b4824bb61cf2ad41c4609852f53bacc
fatima hassan <fatimahasan774@gmail.com>
2026-04-21 07:24:42 +0300

    upload exercise 6

--- ex.js
+++ ex.js
@@ -1,40 +1,7 @@
-// let isSanny =true;
-// let weekend =true;
-
-// console.log(isSanny && weekend);
-
-
-// function add(a,b){
-//         console.log(a+b)
-// }
-// add(20,30);
-// add(100,200);
-
-//exercise 4;
-// function add(a,b){
-//         return a+b
-// }
-// console.log(add (10,10));
-// console.log(add (5,10));
-// console.log(add (8,10));
-// console.log(add (2,1));
-// console.log(add (19,90));
-
-//2 experstion function
-
-// var add =function(a,b){
-//     return a+b
-  
-// }
-
-//  console.log(add(10,10));
-//  console.log(add(20,30));
-//  console.log(add(5,5));
-
- let add = (a,b)=>{
-        console.log(a+b)
- }
- add(10,100)
+let colors =['red', 'white', 'black'];
+console.log(colors[1]);
+console.log(colors[2]);
+console.log(colors[0]);
 
 
 
